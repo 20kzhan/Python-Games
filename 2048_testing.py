8ddf5a1e53ff48d0afb6c9b0b90890e073eb8e07
20kzhan <49377371+20kzhan@users.noreply.github.com>
2021-02-28 12:03:50 -0800

    Finished 2048 Game

---
+++ 2048_testing.py
@@ -0,0 +1,263 @@
+from game_2048 import *
+
+
+
+# test cases for game_over
+# make sure your code can pass these test cases
+
+gb = [[1, 2, 3, 4],
+      [5, 6, 7, 8],
+      [9, 10, 11, 12],
+      [13, 14, 15, 16]]
+
+rotated_board = rotate_board(gb)
+
+for each_row in rotated_board:
+    print(each_row)
+
+print()
+
+gb = [[0, 2, 2, 2],
+      [2, 0, 2, 2],
+      [2, 2, 0, 2],
+      [2, 2, 2, 0]]
+
+shifted_board = shift_board_down(gb)
+
+for each_row in shifted_board:
+    print(each_row)
+
+print('\n')
+
+gb = [[0, 2, 2, 2],
+      [2, 0, 2, 2],
+      [2, 2, 0, 2],
+      [2, 2, 2, 0]]
+
+shifted_board = shift_board_up(gb)
+
+for each_row in shifted_board:
+    print(each_row)
+
+print('\n')
+
+gb = [[0, 2, 4, 8],
+      [2, 4, 8, 2],
+      [4, 8, 2, 4],
+      [8, 2, 4, 2]]
+
+for each_row in gb:
+    print(each_row)
+print("After: ")
+for each_row in shift_board_left(gb):
+    print(each_row)
+print('\n')
+print('\n')
+
+gb = [[4, 2, 4, 8],
+      [2, 0, 8, 2],
+      [4, 8, 2, 4],
+      [8, 2, 4, 2]]
+
+for each_row in gb:
+    print(each_row)
+print("After: ")
+for each_row in shift_board_left(gb):
+    print(each_row)
+print('\n')
+
+gb = [[4, 2, 4, 8],
+      [2, 4, 8, 2],
+      [4, 8, 0, 4],
+      [8, 2, 4, 2]]
+
+for each_row in gb:
+    print(each_row)
+print("After: ")
+for each_row in shift_board_left(gb):
+    print(each_row)
+print('\n')
+
+gb = [[4, 2, 4, 8],
+      [2, 4, 8, 2],
+      [4, 8, 2, 4],
+      [8, 2, 4, 0]]
+
+for each_row in gb:
+    print(each_row)
+print("After: ")
+for each_row in shift_board_left(gb):
+    print(each_row)
+print('\n')
+
+gb = [[0, 2, 4, 8],
+      [2, 4, 8, 2],
+      [4, 8, 2, 4],
+      [8, 2, 4, 0]]
+
+for each_row in gb:
+    print(each_row)
+print("After: ")
+for each_row in shift_board_left(gb):
+    print(each_row)
+print('\n')
+
+gb = [[0, 2, 4, 8],
+      [2, 4, 0, 2],
+      [4, 8, 2, 4],
+      [8, 2, 4, 0]]
+
+for each_row in gb:
+    print(each_row)
+print("After: ")
+for each_row in shift_board_left(gb):
+    print(each_row)
+print('\n')
+
+gb = [[0, 2, 4, 8],
+      [2, 4, 0, 2],
+      [4, 0, 2, 4],
+      [8, 2, 4, 0]]
+
+for each_row in gb:
+    print(each_row)
+print("After: ")
+for each_row in shift_board_left(gb):
+    print(each_row)
+print('\n')
+
+gb = [[4, 2, 4, 8],
+      [2, 4, 8, 2],
+      [4, 8, 2, 4],
+      [8, 2, 4, 2]]
+
+for each_row in gb:
+    print(each_row)
+print("After: ")
+for each_row in shift_board_left(gb):
+    print(each_row)
+print('\n')
+
+gb = [[8, 2, 4, 8],
+      [2, 4, 16, 2],
+      [4, 32, 2, 4],
+      [8, 2, 4, 64]]
+
+for each_row in gb:
+    print(each_row)
+print("After: ")
+for each_row in shift_board_left(gb):
+    print(each_row)
+print('\n')
+
+# test cases for shift_left
+# make sure your code can pass these test cases
+
+sl = [0] * 4
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 0, 0, 0]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [0, 2, 0, 0]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [0, 0, 0, 2]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [0, 2, 0, 4]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [0, 2, 4, 8]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 2, 0, 0]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 0, 2, 0]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 0, 0, 2]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 2, 0, 2]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 0, 2, 2]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 2, 2, 2]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [4, 2, 4, 2]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [0, 2, 2, 0]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [0, 2, 2, 4]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [8, 4, 2, 2]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 2, 8, 4]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [8, 4, 0, 4]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 16, 4, 4]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [4, 16, 4, 4]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 2, 16, 8]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 4, 16, 16]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 2, 0, 32]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 2, 4, 32]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [0, 8, 8, 2]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 8, 8, 0]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
+sl = [2, 0, 8, 8]
+print(f'original list: {sl}')
+print(shift(sl, right=False))
+
