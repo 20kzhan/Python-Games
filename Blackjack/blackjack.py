95837bfbba9aa469a1996b1574dd2b52f372cc17
20kzhan <49377371+20kzhan@users.noreply.github.com>
2021-05-08 21:09:21 -0700

    Add files via upload

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -1,7 +1,8 @@
 import random
 
+
 class Card:
-    CARD_RANKS = ["A░", "2░","3░","4░","5░","6░","7░","8░","9░","10","J░","Q░","K░","░░"]
+    CARD_RANKS = ["A░", "2░", "3░", "4░", "5░", "6░", "7░", "8░", "9░", "10", "J░", "Q░", "K░", "░░"]
     CARD_SUITS = ['♠', '♥', '♣', '♦', '░']
 
     CARD_IMAGE = [
@@ -15,29 +16,27 @@ class Card:
         '└─────────┘\t'
     ]
 
-    CARD_IMAGE_TOP =       '┌─────────┐\t'
-    CARD_IMAGE_MIDDLE =    '│░░░░░░░░░│\t'
-    CARD_IMAGE_LEFT =      '│░'
-    CARD_IMAGE_RIGHT =        '░░░░░░│\t'
+    CARD_IMAGE_TOP = '┌─────────┐\t'
+    CARD_IMAGE_MIDDLE = '│░░░░░░░░░│\t'
+    CARD_IMAGE_LEFT = '│░'
+    CARD_IMAGE_RIGHT = '░░░░░░│\t'
     CARD_IMAGE_SUIT_LEFT = '│░░░░'
-    CARD_IMAGE_SUIT_RIGHT =      '░░░░│\t'
-    CARD_IMAGE_BOTTOM =    '└─────────┘\t'
+    CARD_IMAGE_SUIT_RIGHT = '░░░░│\t'
+    CARD_IMAGE_BOTTOM = '└─────────┘\t'
 
-
-    def __init__(self, value):
+    def __init__(self, value=-1):
         if value > 51 or value < 0:
             value = random.randint(0, 51)
         self._value = value
 
-
-    def get_suit_image(self):
-        return self.CARD_SUITS[self._value // 13]
-
-    def get_value(self):
-        return str(self._value)
+    def get_rank_value(self):
+        return self._value % 13
 
     def get_rank_image(self):
         return self.CARD_RANKS[self._value % 13]
+
+    def get_suit_image(self):
+        return self.CARD_SUITS[self._value // 13]
 
     def __str__(self, covered=False):
         '''
@@ -60,7 +59,24 @@ class Card:
 
         return some_str
 
-class BJ_Hand:
+    def get_card_image(self, covered=False):
+        # will return a list of strings
+        image = []
+        for index, line in enumerate(self.CARD_IMAGE):
+            if index == 2:
+                image.append(line[0] + \
+                            (self.get_rank_image() if not covered else self.CARD_RANKS[-1]) + \
+                            line[1])
+            elif index == 4:
+                image.append(line[0] + \
+                            (self.get_suit_image() if not covered else self.CARD_SUITS[-1]) + \
+                            line[1])
+            else:
+                image.append(line)
+
+        return image
+
+class BJHand:
     # This class implements a hand of BJ cards with following functionality
     #   1. printing the cards out
     #   2. Computes the hand's total value, whether the hand is a bust, etc
@@ -69,114 +85,144 @@ class BJ_Hand:
     def __init__(self, cards=None):
         self._hand = []
 
-    def print(self, msg="", col_size=5, cover_first_card=False):
+    def print(self, col_size=2, cover_first_card=True):
         # prints the entire hand vertically, how many cards across?
-        print(str())
+        card_img_list = []
+        for i in range(len(self._hand)):
+            if i == 0:
+                card_img_list.append(self._hand[i].get_card_image(cover_first_card))
+            else:
+                card_img_list.append(self._hand[i].get_card_image())
+        for x in range(8):
+            test = [i[x] for i in card_img_list]
+            for y in range(len(test)):
+                print(test[y], end='')
+            print()
 
-    def score(self):
-        count = 0
+
+    def get_score(self):
         # returns the value of the entire hand
-        pass
+        value = 0
+        has_ace = False
+        for c in self._hand:
+            value += self.CARD_VALUES[c.get_rank_value()]
+            if c.get_rank_value() == 0:
+                has_ace = True
+
+        if has_ace and value + 10 <= 21:
+            return value + 10
+        return value
 
     def is_bust(self):
         # returns True/False whether the hand is a bust
-        pass
+        return self.get_score() > 21
 
     def get_num_cards(self):
-        pass
+        return len(self._hand)
 
     def add_card(self, card):
-        pass
+        self._hand.append(Card())
 
 
-
-class Card_Deck:
+class CardDeck:
     def __init__(self):
         # This function initializes the deck
         # You should create a list of 52 cards in sequence from 0 to 51
         self._card_list = []
         for i in range(52):
             self._card_list.append(Card(i))
+        self.shuffle()
 
     def shuffle(self):
         # This function shuffles the deck of cards
         # Hint: just shuffle the list of Cards (object)
         random.shuffle(self._card_list)
-        return self._card_list
 
     def deal_card(self):
         # This function deals a card from the deck
         # Just pot a card from the list and return it
         # Did you accidentally misspell pop?..
-        return self._card_list.pop(random.randint(0, 51))
+        return self._card_list.pop()
 
-    def get_cards_remaining(self):
+    def num_cards_remaining(self):
         # This function returns the # of cards left in the deck
-        return self._card_list
+        return len(self._card_list)
 
     def print(self):
         for c in self._card_list:
             print(str(c))
 
-class BJ_Game:
+
+class BJGame:
     def __init__(self):
         pass
 
     def play(self):
         pass
-card_deck1 = Card_Deck()
-while True:
-    card1 = Card(value=-1)
 
-    # print(card1)
-    # input('Press Enter to regenerate card')
-    card_deck1.print()
-    input()
-    card_deck1.shuffle()
+if __name__ == '__main__':
+    card_deck1 = CardDeck()
 
-    '''
-    #########################################
-    # simple test on Card and Card_Deck classes 
-    #########################################
+    player = BJHand()
+    dealer = BJHand()
 
-    # create a new deck of cards
-    deck = Card_Deck()
+    player.add_card((card_deck1.deal_card()))
+    player.add_card((card_deck1.deal_card()))
+    player.add_card((card_deck1.deal_card()))
 
-    card1 = deck.deal_card()
-    card2 = deck.deal_card()
-    card3 = deck.deal_card()
+    player.print()
 
-    card1.print_card(show_back=True)
-    card1.print_card()
-    card2.print_card()
-    card3.print_card()
+    # while True:
+    #     card1 = Card(value=-1)
+    #
+    #     # print(card1)
+    #     # input('Press Enter to regenerate card')
+    #     card_deck1.print()
+    #     input()
+    #     card_deck1.shuffle()
 
-    deck=Card_Deck()
+'''
+#########################################
+# simple test on Card and Card_Deck classes 
+#########################################
 
-    # deal 52 cards and print out each card (should be in order)
-    for i in range(52):
-      print("i:",i)
-      card=deck.deal_card()
-      card.print_card()
-      if not i:
-        card.print_card(show_back=True)
+# create a new deck of cards
+deck = Card_Deck()
 
-    # now shuffle the deck of cards
-    deck.shuffle()
+card1 = deck.deal_card()
+card2 = deck.deal_card()
+card3 = deck.deal_card()
 
-    # deal 52 cards and print out each card (should be shuffled, random)
-    for i in range(52):
-      print("i:",i)
-      card=deck.deal_card()
-      card.print_card()
-      if not i:
-        card.print_card(show_back=True)
+card1.print_card(cover_first_card=True)
+card1.print_card()
+card2.print_card()
+card3.print_card()
 
-    # test the get_card_image() function
-    deck.shuffle()
-    card=deck.deal_card()
-    card_text=card.get_card_image()
-    print("Card Text:")
-    print(card_text)
-    '''
+deck = Card_Deck()
 
+# deal 52 cards and print out each card (should be in order)
+for i in range(52):
+    print("i:", i)
+    card = deck.deal_card()
+    card.print_card()
+    if not i:
+        card.print_card(cover_first_card=True)
+
+# now shuffle the deck of cards
+deck.shuffle()
+
+# deal 52 cards and print out each card (should be shuffled, random)
+for i in range(52):
+    print("i:", i)
+    card = deck.deal_card()
+    card.print_card()
+    if not i:
+        card.print_card(cover_first_card=True)
+
+# test the get_card_image() function
+deck.shuffle()
+card = deck.deal_card()
+card_text = card.get_card_image()
+print("Card Text:")
+print(card_text)
+'''
