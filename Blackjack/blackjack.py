fb452262ebe866faf26b5b1ac30ae83a19b2c3a5
20kzhan <49377371+20kzhan@users.noreply.github.com>
2021-05-24 21:30:38 -0700

    Add files via upload

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -1,6 +1,7 @@
+# coding=ascii
+from __future__ import print_function
 import random
 import os, sys
-
 # for windows OS
 if os.name == "nt":
     os.system("cls")
@@ -21,27 +22,27 @@ if os.name == "nt":
 
 
 class Card:
-    CARD_RANKS = ["A░", "2░", "3░", "4░", "5░", "6░", "7░", "8░", "9░", "10", "J░", "Q░", "K░", "░░"]
-    CARD_SUITS = ['♠', '♥', '♣', '♦', '░']
+    CARD_RANKS = ["A?", "2?", "3?", "4?", "5?", "6?", "7?", "8?", "9?", "10", u"J?", u"Q?", u"K?", u"??"]
+    CARD_SUITS = [u'?', u'?', u'?', u'?', u'?']
 
     CARD_IMAGE = [
-        '┌─────────┐\t',
-        '│░░░░░░░░░│\t',
-        ['│░', '░░░░░░│\t'],
-        '│░░░░░░░░░│\t',
-        ['│░░░░', '░░░░│\t'],
-        '│░░░░░░░░░│\t',
-        '│░░░░░░░░░│\t',
-        '└─────────┘\t'
+        u'???????????\t',
+        u'???????????\t',
+        [u'??', u'???????\t'],
+        u'???????????\t',
+        [u'?????', u'?????\t'],
+        u'???????????\t',
+        u'???????????\t',
+        u'???????????\t'
     ]
 
-    CARD_IMAGE_TOP = '┌─────────┐\t'
-    CARD_IMAGE_MIDDLE = '│░░░░░░░░░│\t'
-    CARD_IMAGE_LEFT = '│░'
-    CARD_IMAGE_RIGHT = '░░░░░░│\t'
-    CARD_IMAGE_SUIT_LEFT = '│░░░░'
-    CARD_IMAGE_SUIT_RIGHT = '░░░░│\t'
-    CARD_IMAGE_BOTTOM = '└─────────┘\t'
+    CARD_IMAGE_TOP = '???????????\t'
+    CARD_IMAGE_MIDDLE = '???????????\t'
+    CARD_IMAGE_LEFT = '??'
+    CARD_IMAGE_RIGHT = '???????\t'
+    CARD_IMAGE_SUIT_LEFT = '?????'
+    CARD_IMAGE_SUIT_RIGHT = '?????\t'
+    CARD_IMAGE_BOTTOM = '???????????\t'
 
     def __init__(self, value=-1):
         if value > 51 or value < 0:
@@ -106,20 +107,18 @@ class BJHand:
         self._player_name = name
         self._hand = []
 
-    def print(self, col_size=2, cover_first_card=False):
+    def name(self):
+        return self._player_name
+
+    def print_hand(self, col_size=2, cover_first_card=False):
         # prints the entire hand vertically, how many cards across?
         card_img_list = []
-        print(f"{self._player_name}'s hand:")
+        print(self._player_name + "'s hand")
         for i in range(len(self._hand)):
             if i == 0:
                 card_img_list.append(self._hand[i].get_card_image(cover_first_card))
             else:
                 card_img_list.append(self._hand[i].get_card_image())
-        # for x in range(8):
-        #     test = [i[x] for i in card_img_list]
-        #     for y in range(len(test)):
-        #         print(test[y], end='')
-        #     print()
         for multiple in range(len(card_img_list) // col_size + 1):
             for image_line_index in range(len(card_img_list[0])):
                 for i in range(col_size):
@@ -129,7 +128,7 @@ class BJHand:
                 print()
         print()
 
-    def get_score(self):
+    def score(self):
         # returns the value of the entire hand
         value = 0
         has_ace = False
@@ -144,9 +143,9 @@ class BJHand:
 
     def is_bust(self):
         # returns True/False whether the hand is a bust
-        return self.get_score() > 21
+        return self.score() > 21
 
-    def get_num_cards(self):
+    def num_cards(self):
         return len(self._hand)
 
     def add_card(self, card=Card()):
@@ -177,10 +176,6 @@ class CardDeck:
         # This function returns the # of cards left in the deck
         return len(self._card_list)
 
-    def print(self):
-        for c in self._card_list:
-            print(str(c))
-
 
 class BJGame:
     def __init__(self):
@@ -196,34 +191,41 @@ class BJGame:
                 state = 1
 
             if state == 1:
+                if deck.num_cards_remaining() <= 4:
+                    print('Card deck is empty, game terminated')
+                    os.system(exit())
                 deck.shuffle()
                 player = BJHand("Player")
                 dealer = BJHand("Dealer")
                 for i in range(2):
                     dealer.add_card(deck.deal_card())
                     player.add_card(deck.deal_card())
-                player.print(cover_first_card=False)
-                dealer.print(cover_first_card=True)
+                player.print_hand(cover_first_card=False)
+                dealer.print_hand(cover_first_card=True)
+                if player.score() == 21:
+                    state = 4
                 state = 2
 
             if state == 2:
-                if player.get_score() == 21:
-                    state = 4
+                if player.num_cards() >= 5:
+                    print('**************{} got 5 cards!**************'.format(player.name()))
+                    print('{} won!'.format(player.name()))
                 if input("Hit or stay? (Enter h or s) ") == 'h':
+                    if deck.num_cards_remaining() == 0:
+                        print('Card deck is empty, game terminated')
+                        os.system(exit())
                     player.add_card(deck.deal_card())
-                    player.print(cover_first_card=False)
-                    dealer.print(cover_first_card=False)
-                    if player.get_score() == 21:
-                        state = 4
-
-                    elif player.is_bust():
+                    player.print_hand(cover_first_card=False)
+                    dealer.print_hand(cover_first_card=True)
+                    if player.score() == 21 or player.is_bust():
                         state = 4
                 else:
                     state = 3
 
             if state == 3:
-                print('b')
-                if dealer.get_score() <= 16:
+                player.print_hand(cover_first_card=False)
+                dealer.print_hand(cover_first_card=False)
+                if dealer.score() <= 16:
                     dealer.add_card(deck.deal_card())
                     if dealer.is_bust():
                         state = 4
@@ -231,26 +233,30 @@ class BJGame:
                     state = 4
 
             if state == 4:
-                player.print(cover_first_card=False)
-                dealer.print(cover_first_card=False)
-                if player.get_score() == dealer.get_score():
+                player.print_hand(cover_first_card=False)
+                dealer.print_hand(cover_first_card=False)
+                if player.is_bust() and dealer.is_bust():
+                    print("Both players bust!")
                     print("It's a tie!\n")
-                elif player.get_score() < dealer.get_score():
-                    print('Dealer won!\n')
-                elif player.get_score() < dealer.get_score():
-                    print('Player won!\n')
-                elif player.get_score() == 21:
+                if player.is_bust():
+                    print('**************{} bust!**************'.format(player.name()))
+                    print('{} won!'.format(dealer.name()))
+                if dealer.is_bust():
+                    print('**************{} bust!**************'.format(dealer.name()))
+                    print('{} won!'.format(player.name))
+                if player.score() == dealer.score():
+                    print("It's a tie!\n")
+                elif player.score() < dealer.score():
+                    print('{} won!\n'.format(dealer.name))
+                elif player.score() < dealer.score():
+                    print('{} won!\n'.format(player.name))
+                elif player.score() == 21:
                     print('**************Blackjack!**************')
-                    print('Player won!\n')
-                elif dealer.get_score() == 21:
+                    print('{} won!\n'.format(player.name))
+                elif dealer.score() == 21:
                     print('**************Blackjack!**************')
-                    print('Dealer won!\n')
-                elif player.is_bust():
-                    print('**************Player bust!**************')
-                    print('Dealer won!')
-                elif dealer.is_bust():
-                    print('**************Dealer bust!**************')
-                    print('Player won!')
+                    print('{} won!\n'.format(dealer.name))
+
                 self.reset_game()
                 state = 1
 
@@ -261,16 +267,16 @@ class BJGame:
     #         self._player.add_card()
     #     else:
     #         player_stand = True
-    #     if self._player.is_bust():
+    #     if self._player.is_bust:
     #         print('**************Player bust!**************')
     #         print('Dealer won!')
     #         self.reset_game()
-    #     elif self._player.get_score() == 21:
+    #     elif self._player.score == 21:
     #         print('**************Blackjack!**************')
     #         print('Player won!\n')
     #         self.reset_game()
     #
-    #     if self._dealer.get_score() < 16:
+    #     if self._dealer.score < 16:
     #         self._dealer.add_card()
     #     elif random.randint(1, 4) == 4:
     #         self._dealer.add_card()
@@ -282,11 +288,11 @@ class BJGame:
     #
     #
     #
-    #     if self._dealer.is_bust():
+    #     if self._dealer.is_bust:
     #         print('**************Dealer bust!**************')
     #         print('Player won!')
     #         self.reset_game()
-    #     elif self._dealer.get_score() == 21:
+    #     elif self._dealer.score == 21:
     #         print('**************Blackjack!**************')
     #         print('Dealer won!\n')
     #         self.reset_game()
@@ -326,21 +332,16 @@ if __name__ == '__main__':
 #########################################
 # simple test on Card and Card_Deck classes 
 #########################################
-
 # create a new deck of cards
 deck = Card_Deck()
-
 card1 = deck.deal_card()
 card2 = deck.deal_card()
 card3 = deck.deal_card()
-
 card1.print_card(cover_first_card=True)
 card1.print_card()
 card2.print_card()
 card3.print_card()
-
 deck = Card_Deck()
-
 # deal 52 cards and print out each card (should be in order)
 for i in range(52):
     print("i:", i)
@@ -348,10 +349,8 @@ for i in range(52):
     card.print_card()
     if not i:
         card.print_card(cover_first_card=True)
-
 # now shuffle the deck of cards
 deck.shuffle()
-
 # deal 52 cards and print out each card (should be shuffled, random)
 for i in range(52):
     print("i:", i)
@@ -359,7 +358,6 @@ for i in range(52):
     card.print_card()
     if not i:
         card.print_card(cover_first_card=True)
-
 # test the get_card_image() function
 deck.shuffle()
 card = deck.deal_card()
