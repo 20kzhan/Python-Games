51ba377330c38f4ed03b7e057790f115e707770c
20kzhan <49377371+20kzhan@users.noreply.github.com>
2021-07-20 13:09:24 -0700

    Update decimal_to_binary.py

--- Random Python Practice/decimal_to_binary.py
+++ Random Python Practice/decimal_to_binary.py
@@ -1,8 +1,8 @@
 def decimal_to_binary(num):
     if num >= 1:
         decimal_to_binary(num // 2)
-    print(num % 2, end='')
+    return num % 2
 
 if __name__ == '__main__':
     dec_val = 24
-    decimal_to_binary(dec_val)
+    print(decimal_to_binary(dec_val))
